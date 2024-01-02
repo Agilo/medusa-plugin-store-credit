caf0fa8183e6d2b77ce32da8fe35c1b9cdd3ebde
Ivo Limić <ilimic1@gmail.com>
2024-01-02 18:20:04 +0100

    adding useAdminStoreCreditsCustomers to admin client.

--- packages/medusa-plugin-store-credit/src/admin/packages/admin-client/src/custom/hooks/bundles/queries.ts
+++ packages/medusa-plugin-store-credit/src/admin/packages/admin-client/src/custom/hooks/bundles/queries.ts
@@ -2,10 +2,12 @@ import { Response } from "@medusajs/medusa-js";
 import { useQuery } from "@tanstack/react-query";
 import { useMedusa } from "medusa-react";
 import qs from "qs";
+import { AdminGetStoreCreditsCustomersParams } from "../../../../../../../api/routes/admin/store-credits/list-customers";
 import {
-  AdminStoreCreditsListRes,
-  // AdminStoreCreditsRes,
   AdminGetStoreCreditsParams,
+  AdminStoreCreditsCustomersListRes,
+  AdminStoreCreditsListRes,
+  AdminStoreCreditsRes,
 } from "../../../generated/models";
 import { UseQueryOptionsWrapper } from "../../types";
 import { queryKeysFactory } from "../utils/queryKeysFactory";
@@ -14,10 +16,10 @@ const ADMIN_STORE_CREDITS_QUERY_KEY = `admin_store_credits` as const;
 
 export const adminStoreCreditKeys = {
   ...queryKeysFactory(ADMIN_STORE_CREDITS_QUERY_KEY),
-  detailProducts(id: string, query?: any) {
+  listCustomers(query?: any) {
     return [
-      ...this.detail(id),
-      "products" as const,
+      ...this.list(query),
+      "customers" as const,
       { ...(query || {}) },
     ] as const;
   },
@@ -62,6 +64,31 @@ export const useAdminStoreCredit = (
   const { data, ...rest } = useQuery(
     adminStoreCreditKeys.detail(id),
     () => client.client.request("GET", `/admin/store-credits/${id}`),
+    options
+  );
+  return { ...data, ...rest } as const;
+};
+
+export const useAdminStoreCreditsCustomers = (
+  query?: AdminGetStoreCreditsCustomersParams,
+  options?: UseQueryOptionsWrapper<
+    Response<AdminStoreCreditsCustomersListRes>,
+    Error,
+    ReturnType<StoreCreditQueryKeys["listCustomers"]>
+  >
+) => {
+  const { client } = useMedusa();
+
+  let path = "/admin/store-credits/customers";
+
+  if (query) {
+    const queryString = qs.stringify(query);
+    path = `/admin/store-credits/customers?${queryString}`;
+  }
+
+  const { data, ...rest } = useQuery(
+    adminStoreCreditKeys.listCustomers(query),
+    () => client.client.request("GET", path),
     options
   );
   return { ...data, ...rest } as const;
