3eb7b580427d8717327b9d8e23654c2770d709d7
Ivo Limić <ilimic1@gmail.com>
2024-01-08 17:58:29 +0100

    implementing customer detail admin screen.

---
+++ packages/medusa-plugin-store-credit/src/admin/packages/admin-ui-custom/ui/src/components/templates/customer-store-credits-table/index.tsx
@@ -0,0 +1,174 @@
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+import { usePagination, useTable } from "react-table";
+import { useAdminStoreCredits } from "../../../../../../admin-client";
+import Table from "../../../../../../admin-ui/ui/src/components/molecules/table";
+import TableContainer from "../../../../../../admin-ui/ui/src/components/organisms/table-container";
+import { useCustomerStoreCreditsColumns } from "./use-customer-store-credits-columns";
+
+const LIMIT = 15;
+
+type Props = {
+  customerId: string;
+  regionId: string;
+};
+
+const CustomerStoreCreditsTable = ({ customerId, regionId }: Props) => {
+  const { t } = useTranslation();
+  // const [selectedOrderForTransfer, setSelectedOrderForTransfer] =
+  //   useState<Order | null>(null)
+
+  const [offset, setOffset] = useState(0);
+
+  const { store_credits, isLoading, count } = useAdminStoreCredits({
+    customer_id: customerId,
+    region_id: regionId,
+    expand: "region",
+    offset: offset,
+    limit: LIMIT,
+  });
+
+  // const { store_credits, isLoading, count } = useAdminCustomerStoreCredits(
+  //   id,
+  //   {
+  //     // customer_id: id!,
+  //     offset: offset,
+  //     limit: LIMIT,
+  //   },
+  //   {
+  //     keepPreviousData: true,
+  //   }
+  // );
+
+  const columns = useCustomerStoreCreditsColumns();
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    canPreviousPage,
+    canNextPage,
+    pageCount,
+    nextPage,
+    previousPage,
+    // Get the state from the instance
+    state: { pageIndex },
+  } = useTable(
+    {
+      columns,
+      data: store_credits || [],
+      manualPagination: true,
+      initialState: {
+        pageSize: LIMIT,
+        pageIndex: Math.floor(offset / LIMIT),
+      },
+      pageCount: Math.ceil((count || 0) / LIMIT),
+      autoResetPage: false,
+    },
+    usePagination
+  );
+
+  // console.log("canPreviousPage", canPreviousPage);
+  // console.log("canNextPage", canNextPage);
+  // console.log("pageCount", pageCount);
+  // console.log("offset", offset);
+  // console.log("pageIndex", Math.floor(offset / LIMIT));
+  // console.log("pageCount", Math.ceil((count || 0) / LIMIT));
+
+  const handleNext = () => {
+    if (canNextPage) {
+      setOffset(offset + LIMIT);
+      nextPage();
+    }
+  };
+
+  const handlePrev = () => {
+    if (canPreviousPage) {
+      setOffset(offset - LIMIT);
+      previousPage();
+    }
+  };
+
+  return (
+    <>
+      <TableContainer
+        hasPagination
+        isLoading={isLoading}
+        pagingState={{
+          count: count!,
+          offset,
+          pageSize: offset + rows.length,
+          title: t("customer-orders-table-orders", "Orders"),
+          currentPage: pageIndex + 1,
+          pageCount: pageCount,
+          nextPage: handleNext,
+          prevPage: handlePrev,
+          hasNext: canNextPage,
+          hasPrev: canPreviousPage,
+        }}
+      >
+        <Table {...getTableProps()}>
+          <Table.Head>
+            {headerGroups.map((headerGroup) => {
+              return (
+                <Table.HeadRow {...headerGroup.getHeaderGroupProps()}>
+                  {headerGroup.headers.map((column) => {
+                    return (
+                      <Table.HeadCell {...column.getHeaderProps()}>
+                        {column.render("Header")}
+                      </Table.HeadCell>
+                    );
+                  })}
+                </Table.HeadRow>
+              );
+            })}
+          </Table.Head>
+          <Table.Body {...getTableBodyProps()}>
+            {rows.map((row) => {
+              prepareRow(row);
+              return (
+                <Table.Row
+                  forceDropdown
+                  // actions={
+                  //   [
+                  //     {
+                  //       label: t(
+                  //         "customer-orders-table-transfer-order",
+                  //         "Transfer order"
+                  //       ),
+                  //       icon: <RefreshIcon size={"20"} />,
+                  //       onClick: () => {
+                  //         setSelectedOrderForTransfer(row.original as Order)
+                  //       },
+                  //     },
+                  //   ]
+                  // }
+                  {...row.getRowProps()}
+                  linkTo={`/a/store-credits/${row.original.id}`}
+                >
+                  {row.cells.map((cell) => {
+                    return (
+                      <Table.Cell {...cell.getCellProps()}>
+                        {cell.render("Cell")}
+                      </Table.Cell>
+                    );
+                  })}
+                </Table.Row>
+              );
+            })}
+          </Table.Body>
+        </Table>
+      </TableContainer>
+      {/* {selectedOrderForTransfer && (
+        <TransferOrdersModal
+          onDismiss={() => setSelectedOrderForTransfer(null)}
+          order={selectedOrderForTransfer}
+        />
+      )} */}
+    </>
+  );
+};
+
+export default CustomerStoreCreditsTable;
---
+++ packages/medusa-plugin-store-credit/src/admin/packages/admin-ui-custom/ui/src/components/templates/customer-store-credits-table/use-customer-store-credits-columns.tsx
@@ -0,0 +1,56 @@
+import { Order } from "@medusajs/medusa";
+import moment from "moment";
+import { useMemo } from "react";
+import { useTranslation } from "react-i18next";
+import { Column } from "react-table";
+import { StoreCredit } from "../../../../../../../../models/store-credit";
+import { formatAmountWithSymbol } from "../../../../../../admin-ui/ui/src/utils/prices";
+
+export const useCustomerStoreCreditsColumns = (): Column<Order>[] => {
+  const { t } = useTranslation();
+  const columns = useMemo(() => {
+    return [
+      {
+        Header: t("customer-store-credits-table-order", "Original Amount"),
+        accessor: "value",
+        Cell: ({ row, cell: { value } }) => {
+          return formatAmountWithSymbol({
+            amount: value,
+            currency: row.original.region.currency_code,
+            digits: 2,
+          });
+        },
+      },
+      {
+        Header: t("customer-store-credits-table-balance", "Balance"),
+        accessor: "balance",
+        Cell: ({ row, cell: { value } }) =>
+          formatAmountWithSymbol({
+            amount: value,
+            currency: row.original.region.currency_code,
+            digits: 2,
+          }),
+      },
+      {
+        Header: t("customer-store-credits-table-region", "Region"),
+        accessor: "region.name",
+      },
+      {
+        Header: t("customer-store-credits-table-expiry", "Expiry"),
+        accessor: "ends_at",
+        Cell: ({ value }) => {
+          return value ? moment(value).format("DD MMM YYYY hh:mm") : "Never";
+        },
+      },
+      {
+        Header: t("customer-store-credits-table-created", "Created"),
+        accessor: "created_at",
+        Cell: ({ value }) => {
+          return moment(value).format("DD MMM YYYY hh:mm");
+        },
+      },
+    ] as Column<StoreCredit>[];
+  }, []);
+
+  return columns;
+};
