46511fbe5156c27dc41701afa2e56cfae7ba2ae1
Ivo Limić <ilimic1@gmail.com>
2024-01-08 17:48:36 +0100

    adding /admin/store-credit-transactions/ api endpoint.

---
+++ packages/medusa-plugin-store-credit/src/api/routes/admin/store-credit-transactions/index.ts
@@ -0,0 +1,66 @@
+import {
+  PaginatedResponse,
+  authenticate,
+  transformQuery,
+  wrapHandler,
+} from "@medusajs/medusa";
+import cors from "cors";
+import { Router } from "express";
+import { parseCorsOrigins } from "medusa-core-utils";
+import { StoreCredit } from "../../../../models/store-credit";
+import { AdminGetStoreCreditTransactionsParams } from "./list-store-credit-transactions";
+
+export default function adminRoutes(router: Router, admin_cors: string) {
+  const adminRouter = Router();
+
+  router.use("/admin/store-credit-transactions", adminRouter);
+
+  adminRouter.use(
+    cors({
+      origin: parseCorsOrigins(admin_cors),
+      credentials: true,
+    })
+  );
+  adminRouter.use(authenticate());
+
+  adminRouter.get(
+    "/",
+    transformQuery(AdminGetStoreCreditTransactionsParams, {
+      isList: true,
+    }),
+    wrapHandler(require("./list-store-credit-transactions").default)
+  );
+}
+
+/**
+ * @schema AdminStoreCreditsListRes
+ * type: object
+ * x-expanded-relations:
+ *   field: store_credit_transactions
+ *   relations:
+ *     - customer
+ *     - region
+ * required:
+ *   - store_credit_transactions
+ *   - count
+ *   - offset
+ *   - limit
+ * properties:
+ *   store_credit_transactions:
+ *     type: array
+ *     description: An array of Store Credit details.
+ *     items:
+ *       $ref: "#/components/schemas/StoreCredit"
+ *   count:
+ *     type: integer
+ *     description: The total number of items available
+ *   offset:
+ *     type: integer
+ *     description: The number of store credits skipped.
+ *   limit:
+ *     type: integer
+ *     description: The number of items per page
+ */
+export type AdminStoreCreditsListRes = PaginatedResponse & {
+  store_credit_transactions: StoreCredit[];
+};
---
+++ packages/medusa-plugin-store-credit/src/api/routes/admin/store-credit-transactions/list-store-credit-transactions.ts
@@ -0,0 +1,69 @@
+import { Type } from "class-transformer";
+import { IsNumber, IsOptional } from "class-validator";
+import StoreCreditService from "../../../../services/store-credit";
+
+/**
+ * @oas [get] /admin/store-credit-transactions
+ * operationId: AdminGetStoreCreditTransactions
+ * summary: List Store Credit Transactions
+ * description: |
+ *   Retrieves a list of store credit transactions.
+ * x-authenticated: true
+ * parameters:
+ *   - (query) offset=0 {integer} The number of store credits to skip when retrieving the store credits.
+ *   - (query) limit=10 {integer} Limit the number of store credits returned.
+ * x-codegen:
+ *   method: list
+ *   queryParams: AdminGetStoreCreditTransactionsParams
+ * security:
+ *   - api_token: []
+ *   - cookie_auth: []
+ * tags:
+ *   - StoreCredits
+ * responses:
+ *   200:
+ *     description: OK
+ *     content:
+ *       application/json:
+ *         schema:
+ *           $ref: "#/components/schemas/AdminStoreCreditsListRes"
+ */
+export default async (req, res) => {
+  const { skip, take } = req.listConfig;
+
+  const storeCreditService: StoreCreditService =
+    req.scope.resolve("storeCreditService");
+
+  const [store_credit_transactions, count] =
+    await storeCreditService.listAndCount(req.filterableFields, req.listConfig);
+
+  res.json({
+    store_credit_transactions,
+    count,
+    offset: skip,
+    limit: take,
+  });
+};
+
+export class AdminGetStoreCreditTransactionsParams {
+  // @IsString()
+  // @IsOptional()
+  // @Type(() => String)
+  // q?: string;
+
+  @IsNumber()
+  @IsOptional()
+  @Type(() => Number)
+  offset?: number = 0;
+
+  @IsNumber()
+  @IsOptional()
+  @Type(() => Number)
+  limit?: number = 10;
+
+  // @IsString()
+  // @IsOptional()
+  // @IsNotEmpty()
+  // @Type(() => String)
+  // product_id?: string;
+}
