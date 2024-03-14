7173525cccd15a60dfc4c1076386fe11818e9a8d
Ivo Limić <ilimic1@gmail.com>
2024-03-14 09:21:53 +0100

    reworked store credit transactions table.

--- packages/medusa-plugin-store-credit/src/admin/packages/admin-ui-custom/ui/src/components/templates/store-credit-transactions-table/index.tsx
+++ packages/medusa-plugin-store-credit/src/admin/packages/admin-ui-custom/ui/src/components/templates/store-credit-transactions-table/index.tsx
@@ -1,165 +1,48 @@
-import { useState } from "react";
-import { useTranslation } from "react-i18next";
-import { usePagination, useTable } from "react-table";
-import { useAdminStoreCreditTransactions } from "../../../../../../admin-client";
+import moment from "moment";
+import { StoreCredit } from "../../../../../../admin-client";
 import Table from "../../../../../../admin-ui/ui/src/components/molecules/table";
 import TableContainer from "../../../../../../admin-ui/ui/src/components/organisms/table-container";
-import { useCustomerStoreCreditsColumns } from "./use-customer-store-credits-columns";
-
-const LIMIT = 15;
+import { formatAmountWithSymbol } from "../../../../../../admin-ui/ui/src/utils/prices";
 
 type Props = {
-  id: string;
+  storeCredit: StoreCredit;
 };
 
-const StoreCreditTransactionsTable = ({ id }: Props) => {
-  const { t } = useTranslation();
-  // const [selectedOrderForTransfer, setSelectedOrderForTransfer] =
-  //   useState<Order | null>(null)
-
-  const [offset, setOffset] = useState(0);
-  const { store_credit_transactions, isLoading, count } =
-    useAdminStoreCreditTransactions({
-      limit: LIMIT,
-      offset: offset,
-      store_credit_id: id!,
-    });
-  // const { orders, isLoading, count } = useAdminOrders(
-  //   {
-  //     customer_id: id!,
-  //     offset: offset,
-  //     limit: LIMIT,
-  //     // TODO: expanding items is currently not supported by the API, re-enable when it is.
-  //     // expand: "items",
-  //   },
-  //   {
-  //     keepPreviousData: true,
-  //   }
-  // )
-
-  const columns = useCustomerStoreCreditsColumns();
-
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-    canPreviousPage,
-    canNextPage,
-    pageCount,
-    nextPage,
-    previousPage,
-    // Get the state from the instance
-    state: { pageIndex },
-  } = useTable(
-    {
-      columns,
-      data: store_credit_transactions || [],
-      manualPagination: true,
-      initialState: {
-        pageSize: LIMIT,
-        pageIndex: Math.floor(offset / LIMIT),
-      },
-      pageCount: Math.ceil((count || 0) / LIMIT),
-      autoResetPage: false,
-    },
-    usePagination
-  );
-
-  const handleNext = () => {
-    if (canNextPage) {
-      setOffset(offset + LIMIT);
-      nextPage();
-    }
-  };
-
-  const handlePrev = () => {
-    if (canPreviousPage) {
-      setOffset(offset - LIMIT);
-      previousPage();
-    }
-  };
+const StoreCreditTransactionsTable = ({ storeCredit }: Props) => {
+  const transactions = storeCredit.store_credit_transactions || [];
 
   return (
     <>
-      <TableContainer
-        hasPagination
-        isLoading={isLoading}
-        pagingState={{
-          count: count!,
-          offset,
-          pageSize: offset + rows.length,
-          title: t(
-            "customer-store-credits-transactions-table-store-credit-transactions",
-            "Store Credit Transactions"
-          ),
-          currentPage: pageIndex + 1,
-          pageCount: pageCount,
-          nextPage: handleNext,
-          prevPage: handlePrev,
-          hasNext: canNextPage,
-          hasPrev: canPreviousPage,
-        }}
-      >
-        <Table {...getTableProps()}>
+      <TableContainer hasPagination={false} pagingState={undefined}>
+        <Table>
           <Table.Head>
-            {headerGroups.map((headerGroup) => {
-              return (
-                <Table.HeadRow {...headerGroup.getHeaderGroupProps()}>
-                  {headerGroup.headers.map((column) => {
-                    return (
-                      <Table.HeadCell {...column.getHeaderProps()}>
-                        {column.render("Header")}
-                      </Table.HeadCell>
-                    );
-                  })}
-                </Table.HeadRow>
-              );
-            })}
+            <Table.HeadRow>
+              <Table.HeadCell>Date</Table.HeadCell>
+              <Table.HeadCell>Order</Table.HeadCell>
+              <Table.HeadCell>Amount</Table.HeadCell>
+            </Table.HeadRow>
           </Table.Head>
-          <Table.Body {...getTableBodyProps()}>
-            {rows.map((row) => {
-              prepareRow(row);
+          <Table.Body>
+            {transactions.map((transaction) => {
               return (
-                <Table.Row
-                  forceDropdown
-                  // actions={
-                  //   [
-                  //     {
-                  //       label: t(
-                  //         "customer-store-credits-transactions-table-transfer-order",
-                  //         "Transfer order"
-                  //       ),
-                  //       icon: <RefreshIcon size={"20"} />,
-                  //       onClick: () => {
-                  //         setSelectedOrderForTransfer(row.original as Order)
-                  //       },
-                  //     },
-                  //   ]
-                  // }
-                  {...row.getRowProps()}
-                  linkTo={`/a/store-credits/${row.original.id}`}
-                >
-                  {row.cells.map((cell) => {
-                    return (
-                      <Table.Cell {...cell.getCellProps()}>
-                        {cell.render("Cell")}
-                      </Table.Cell>
-                    );
-                  })}
+                <Table.Row>
+                  <Table.Cell>
+                    {moment(transaction.created_at).format("DD MMM YYYY hh:mm")}
+                  </Table.Cell>
+                  <Table.Cell>{transaction.order_id}</Table.Cell>
+                  <Table.Cell>
+                    {formatAmountWithSymbol({
+                      amount: transaction.amount,
+                      currency: storeCredit.region.currency_code,
+                      digits: 2,
+                    })}
+                  </Table.Cell>
                 </Table.Row>
               );
             })}
           </Table.Body>
         </Table>
       </TableContainer>
-      {/* {selectedOrderForTransfer && (
-        <TransferOrdersModal
-          onDismiss={() => setSelectedOrderForTransfer(null)}
-          order={selectedOrderForTransfer}
-        />
-      )} */}
     </>
   );
 };
--- packages/medusa-plugin-store-credit/src/admin/packages/admin-ui-custom/ui/src/domain/store-credits/details/index.tsx
+++ packages/medusa-plugin-store-credit/src/admin/packages/admin-ui-custom/ui/src/domain/store-credits/details/index.tsx
@@ -1,5 +1,4 @@
 import moment from "moment";
-import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useNavigate, useParams } from "react-router-dom";
 import {
@@ -19,6 +18,7 @@ import useNotification from "../../../../../../admin-ui/ui/src/hooks/use-notific
 import useToggleState from "../../../../../../admin-ui/ui/src/hooks/use-toggle-state";
 import { getErrorMessage } from "../../../../../../admin-ui/ui/src/utils/error-messages";
 import { getErrorStatus } from "../../../../../../admin-ui/ui/src/utils/get-error-status";
+import StoreCreditTransactionsTable from "../../../components/templates/store-credit-transactions-table";
 import EditStoreCreditModal from "./edit-store-credit-modal";
 import UpdateBalanceModal from "./update-balance-modal";
 
@@ -31,9 +31,6 @@ const StoreCreditDetail = () => {
   });
   const updateStoreCredit = useAdminUpdateStoreCredit(store_credit?.id);
   const { t } = useTranslation();
-  const [showEdit, setShowEdit] = useState(false);
-
-  console.log("store_credit", store_credit);
 
   const storeCreditName = `Store Credit: ${
     store_credit
@@ -70,8 +67,6 @@ const StoreCreditDetail = () => {
     },
   ];
 
-  // const { getWidgets } = useWidgets()
-
   const notification = useNotification();
 
   const updateStatus = (data: { is_disabled?: boolean }) => {
@@ -84,18 +79,18 @@ const StoreCreditDetail = () => {
             t("details-updated-status", "Updated status"),
             t(
               "details-successfully-updated-the-status-of-the-gift-card",
-              "Successfully updated the status of the Gift Card"
+              "Successfully updated the status of the Gift Card",
             ),
-            "success"
+            "success",
           );
         },
         onError: (err) =>
           notification(
             t("details-error", "Error"),
             getErrorMessage(err),
-            "error"
+            "error",
           ),
-      }
+      },
     );
   };
 
@@ -130,17 +125,6 @@ const StoreCreditDetail = () => {
         className="mb-xsmall"
       />
       <div className="gap-y-xsmall flex flex-col">
-        {/* {getWidgets("customer.details.before").map((w, i) => {
-          return (
-            <WidgetContainer
-              key={i}
-              entity={customer}
-              injectionZone="customer.details.before"
-              widget={w}
-            />
-          )
-        })} */}
-
         <BodyCard
           compact={true}
           title={storeCreditName}
@@ -157,20 +141,6 @@ const StoreCreditDetail = () => {
           actionables={actions}
           forceDropdown={true}
         >
-          {/* <div className="flex w-full items-start justify-between">
-            <div className="gap-x-base flex w-full items-center">
-              
-              <div className="flex grow flex-col">
-                <h1 className="inter-xlarge-semibold text-grey-90 max-w-[50%] truncate">
-                  {storeCreditName}
-                </h1>
-                <h3 className="inter-small-regular text-grey-50">
-                  {store_credit.customer.email}
-                </h3>
-              </div>
-            </div>
-            <Actionables actions={actions} forceDropdown />
-          </div> */}
           <div className="flex justify-between">
             <div className="flex space-x-6 divide-x">
               <div className="flex flex-col">
@@ -241,28 +211,17 @@ const StoreCreditDetail = () => {
         <BodyCard
           title={t(
             "store-credit-detail-transactions",
-            "Store Credit Transactions"
+            "Store Credit Transactions",
           )}
           subtitle={t(
             "store-credit-detail-an-overview-of-transactions",
-            "List of store credit transactions"
+            "List of store credit transactions",
           )}
         >
           <div className="flex  grow flex-col">
-            {/* <StoreCreditTransactionsTable id={id} /> */}
+            <StoreCreditTransactionsTable storeCredit={store_credit} />
           </div>
         </BodyCard>
-
-        {/* {getWidgets("customer.details.after").map((w, i) => {
-          return (
-            <WidgetContainer
-              key={i}
-              entity={customer}
-              injectionZone="customer.details.after"
-              widget={w}
-            />
-          )
-        })} */}
 
         <RawJSON
           data={store_credit}
